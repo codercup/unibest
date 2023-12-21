da0a64c535f655da31efa5f7d09290b4ee25a8fb
Burt <1020103647@qq.com>
2023-12-21 17:32:53 +0800

    build: 续上

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
+import 'virtual:svg-icons-register'
+import 'virtual:uno.css'
+
 export function createApp() {
   const app = createSSRApp(App)
   return {
