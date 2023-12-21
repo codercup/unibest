47c8ac2b5d88ecb6789e94f9b2ad3ef64faa8023
Burt <1020103647@qq.com>
2023-12-21 15:33:50 +0800

    build(prettier): add .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// @see https://prettier.io/docs/en/options
+module.exports = {
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+}
