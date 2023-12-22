cae7de9d174afb91dab76875cce1ea68ac4e7379
Burt <1020103647@qq.com>
2023-12-22 09:15:09 +0800

    build(vscode): extensions.json

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,7 +7,7 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
+    RouterLink: (typeof import('vue-router'))['RouterLink']
+    RouterView: (typeof import('vue-router'))['RouterView']
   }
 }
