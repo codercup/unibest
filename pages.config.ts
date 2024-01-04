a1d3cf34427e887130451ffb2252cc4c4f05b237
Burt <1020103647@qq.com>
2024-01-04 09:59:21 +0800

    build(pages): 精简pages.config.ts配置

--- pages.config.ts
+++ pages.config.ts
@@ -8,7 +8,6 @@ export default defineUniPages({
   easycom: {
     autoscan: true,
     custom: {
-      '^Fly(.*)': '@/components/fly-$1/fly-$1.vue',
       '^fly-(.*)': '@/components/fly-$1/fly-$1.vue',
       '^uni-(.*)': '@dcloudio/uni-ui/lib/uni-$1/uni-$1.vue',
     },
@@ -37,32 +36,6 @@ export default defineUniPages({
       },
     ],
   },
-  /**
-   * 设置默认路由入口
-   * @default 'pages/index' || 'pages/index/index'
-   */
-  // homePage: 'pages/index/index',
-  /**
-   * 是否扫描并合并 pages.json 中 pages 字段
-   * @default true
-   */
-  // mergePages: true,
-
-  /**
-   * 扫描的目录
-   * @default 'src/pages'
-   */
-  // dir: 'src/pages',
-  /**
-   * 自定义块语言
-   * @default 'json5' 可选值：'json5' | 'json' | 'yaml' | 'yml',
-   */
-  // routeBlockLang: 'json5', // 注意json5允许注释，单行多行都可以，还有其他很多特性，推荐使用
-  /**
-   * 排除的页面，相对于 dir 和 subPackages，通常把这些目录里面的components里面的文件排除掉
-   * @default []
-   */
-  exclude: ['**/components/**/*.*'],
   // 你也可以定义 pages 字段，它具有最高的优先级。
   pages: [],
   /**
