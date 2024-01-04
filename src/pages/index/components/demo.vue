aefa43c5ebfff2261178d7b882fba330b7afd15b
Burt <1020103647@qq.com>
2024-01-04 10:05:14 +0800

    feat(pages): pages里components里面的文件也会注册为pages页面, 不妥

--- src/pages/index/components/demo.vue
+++ src/pages/index/components/demo.vue
@@ -1,3 +1,8 @@
 <template>
-  <view> demo </view>
+  <view>
+    <view>demo</view>
+    <view>测试是否会自动引入到pages，发现会</view>
+    <view>说明components里面的vue文件也会注册为page，这个特性感觉不太好</view>
+    <view>我已经在pages.config.ts里面配置了exclude，但是没生效</view>
+  </view>
 </template>
