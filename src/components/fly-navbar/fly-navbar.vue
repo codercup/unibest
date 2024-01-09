2878a4fe22624ab631ab20716e213180d61dfa80
Burt <1020103647@qq.com>
2024-01-09 09:22:24 +0800

    Revert "feat: navbar 图标加圆形背景色"
    This reverts commit c75a17375aaa4f7ca9e6319ce4b18688b06c862d.

--- src/components/fly-navbar/fly-navbar.vue
+++ src/components/fly-navbar/fly-navbar.vue
@@ -3,9 +3,7 @@
   <view class="fly-navbar" :style="{ paddingTop: safeAreaInsets?.top + 'px' }">
     <!-- 1/3，多于1个页面，用返回图标 -->
     <navigator v-if="pages.length > 1" open-type="navigateBack" class="left-icon">
-      <view class="bg-gray-500/80 rounded-full w-8 h-8 flex items-center justify-center">
-        <button class="i-carbon-chevron-left text-white w-7 h-7"></button>
-      </view>
+      <button class="i-carbon-chevron-left text-white"></button>
     </navigator>
     <!-- 2/3，只有1个页面，如果不是tabbar，需要首页图标 -->
     <!-- 这种情况一般出现在用户直接打开分享出去的详情页面，或者使用redirectTo等API -->
@@ -15,9 +13,7 @@
       url="/pages/index/index"
       class="left-icon"
     >
-      <view class="bg-gray-500/80 rounded-full w-8 h-8 flex items-center justify-center">
-        <button class="i-carbon-home text-white w-6 h-6"></button>
-      </view>
+      <button class="i-carbon-home text-white"></button>
     </navigator>
     <!-- 3/3，如果当前页就是tabbar页，不用去首页，也就是什么图标都不需要 -->
     <view class="title">{{ title || '' }}</view>
@@ -31,7 +27,6 @@ defineProps<{ title?: string }>()
 // 获取页面栈
 const pages = getCurrentPages()
 const isTabbar = getIsTabbar()
-console.log({ isTabbar, pagesLen: pages.length })
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,11 +4,10 @@ console.log(pagesJson)
 
 /** 判断当前页面是否是tabbar页  */
 export const getIsTabbar = () => {
-  if (!pagesJson.tabBar || !pagesJson.tabBar.list.length) {
+  if (!pagesJson.tabBar) {
     return false
   }
   const pages = getCurrentPages()
   const currPath = pages.at(-1).route
-  console.log(currPath)
   return !!pagesJson.tabBar.list.find((e) => e.pagePath === currPath)
 }
