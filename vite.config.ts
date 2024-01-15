47b85322baa5c0ba93ca6276b8996bee9420ebf6
Burt <1020103647@qq.com>
2024-01-15 10:43:52 +0800

    feat: 先去掉 vite-plugin-imagemin， 很多人安装失败

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import viteCompression from 'vite-plugin-compression'
 import ViteRestart from 'vite-plugin-restart'
 import { visualizer } from 'rollup-plugin-visualizer'
-import viteImagemin from 'vite-plugin-imagemin'
+// import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default ({ command, mode }) => {
@@ -79,40 +79,40 @@ export default ({ command, mode }) => {
           brotliSize: true,
         }),
       // 这个图片压缩插件比较耗时，希望仅在生产环境使用
-      mode === 'production' &&
-        viteImagemin({
-          gifsicle: {
-            // gif图片压缩
-            optimizationLevel: 3, // 选择1到3之间的优化级别
-            interlaced: false, // 隔行扫描gif进行渐进式渲染
-            // colors: 2 // 将每个输出GIF中不同颜色的数量减少到num或更少。数字必须介于2和256之间。
-          },
-          optipng: {
-            // png
-            optimizationLevel: 7, // 选择0到7之间的优化级别
-          },
-          mozjpeg: {
-            // jpeg
-            quality: 20, // 压缩质量，范围从0(最差)到100(最佳)。
-          },
-          pngquant: {
-            // png
-            quality: [0.8, 0.9], // Min和max是介于0(最差)到1(最佳)之间的数字，类似于JPEG。达到或超过最高质量所需的最少量的颜色。如果转换导致质量低于最低质量，图像将不会被保存。
-            speed: 4, // 压缩速度，1(强力)到11(最快)
-          },
-          svgo: {
-            // svg压缩
-            plugins: [
-              {
-                name: 'removeViewBox',
-              },
-              {
-                name: 'removeEmptyAttrs',
-                active: false,
-              },
-            ],
-          },
-        }),
+      // mode === 'production' &&
+      //   viteImagemin({
+      //     gifsicle: {
+      //       // gif图片压缩
+      //       optimizationLevel: 3, // 选择1到3之间的优化级别
+      //       interlaced: false, // 隔行扫描gif进行渐进式渲染
+      //       // colors: 2 // 将每个输出GIF中不同颜色的数量减少到num或更少。数字必须介于2和256之间。
+      //     },
+      //     optipng: {
+      //       // png
+      //       optimizationLevel: 7, // 选择0到7之间的优化级别
+      //     },
+      //     mozjpeg: {
+      //       // jpeg
+      //       quality: 20, // 压缩质量，范围从0(最差)到100(最佳)。
+      //     },
+      //     pngquant: {
+      //       // png
+      //       quality: [0.8, 0.9], // Min和max是介于0(最差)到1(最佳)之间的数字，类似于JPEG。达到或超过最高质量所需的最少量的颜色。如果转换导致质量低于最低质量，图像将不会被保存。
+      //       speed: 4, // 压缩速度，1(强力)到11(最快)
+      //     },
+      //     svgo: {
+      //       // svg压缩
+      //       plugins: [
+      //         {
+      //           name: 'removeViewBox',
+      //         },
+      //         {
+      //           name: 'removeEmptyAttrs',
+      //           active: false,
+      //         },
+      //       ],
+      //     },
+      //   }),
     ],
     css: {
       postcss: {
