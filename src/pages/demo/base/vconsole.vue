cf978ac361b6847908a3fafa691aee82f42c2e32
Burt <1020103647@qq.com>
2024-01-22 19:18:17 +0800

    feat: 微信小程序 vconsole

---
+++ src/pages/demo/base/vconsole.vue
@@ -0,0 +1,22 @@
+<route lang="json5">
+{
+  layout: 'demo',
+  style: { navigationBarTitleText: '开启 vConsole' },
+}
+</route>
+
+<template>
+  <view class="text-5 h-8 leading-8">
+    <view class="text-red-500">在非正式版小程序里面已经集成了VConsole</view>
+    <view class="text-blue-500 mb-2">开启方式如下面</view>
+    <image
+      src="https://cip-shopping-page-0eysug01066a9e-1302818703.tcloudbaseapp.com/fly/vconsole/1.png"
+      mode="widthFix"
+    />
+    <view class="text-blue-500 m-2">然后页面上会出现一个 `vConsole` 的调试按钮，如下图</view>
+    <image
+      src="https://cip-shopping-page-0eysug01066a9e-1302818703.tcloudbaseapp.com/fly/vconsole/2.png"
+      mode="widthFix"
+    />
+  </view>
+</template>
