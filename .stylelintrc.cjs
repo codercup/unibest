d237bf8b1a92dcf7bd581a5004dbaeeb641b4679
Burt <1020103647@qq.com>
2024-01-23 10:49:37 +0800

    feat: 优化v3代码块

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -45,5 +45,6 @@ module.exports = {
     'comment-empty-line-before': 'never',
     'custom-property-empty-line-before': 'never',
     'no-empty-source': null,
+    'scss/comment-no-empty': null,
   },
 }
