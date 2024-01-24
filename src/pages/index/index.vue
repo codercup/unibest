a6301e18157d44eac1b0da5abba80fd6b5280517
Burt <1020103647@qq.com>
2024-01-24 11:09:20 +0800

    docs: 开发模板 --> 开发框架

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -10,7 +10,7 @@
       <image src="/static/logo.svg" alt="" class="w-40 h-40 block mx-auto" />
     </view>
     <view class="text-center text-4xl main-title-color mt-4">unibest</view>
-    <view class="text-center text-2xl mt-4 mb-8">最好用的 uniapp 开发模板</view>
+    <view class="text-center text-2xl mt-4 mb-8">最好用的 uniapp 开发框架</view>
     <view class="text-center text-xl">
       unibest 是由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI 驱动的跨端快速启动模板，使用 VS Code
       开发，具有代码提示、自动格式化、统一配置、代码片段等功能，同时内置了大量平时开发常用的基本组件，开箱即用，让你编写
