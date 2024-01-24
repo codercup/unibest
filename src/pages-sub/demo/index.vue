8a16c4b61fb13275aa1661353634bd7ac1d71acf
Burt <1020103647@qq.com>
2024-01-24 14:32:10 +0800

    fix: 设置了分包，如果没有路由会报错

---
+++ src/pages-sub/demo/index.vue
@@ -0,0 +1,17 @@
+<route lang="json5" type="page">
+{
+  style: { navigationBarTitleText: '分包-DEMO' },
+}
+</route>
+
+<template>
+  <view class="">分包-DEMO</view>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
