314f7acc9b1fd78b8ffe23d1a80214a6f3e6709a
菲鸽 <1020103647@qq.com>
2024-01-30 20:33:59 +0800

    feat: uvui 引入

--- src/pages-sub/demo/index.vue
+++ src/pages-sub/demo/index.vue
@@ -9,6 +9,18 @@
     <view class="m-8"> http://localhost:9000/#/pages-sub/demo/index </view>
     <view class="text-green-500"> 分包页面demo </view>
     <view class="test">test global css</view>
+    <view class="text-green-500 text-xl mt-10">uvui 引入演示</view>
+    <view class="center my-10">
+      <uv-icon name="photo" class="text-center m-auto"></uv-icon>
+      <uv-icon name="photo" class="text-center m-auto"></uv-icon>
+      <uv-icon name="photo" class="text-center m-auto"></uv-icon>
+    </view>
+    <uv-qrcode
+      ref="qrcode"
+      size="200px"
+      class="m-auto"
+      value="https://gitee.com/codercup/unibest"
+    ></uv-qrcode>
   </view>
 </template>
 
