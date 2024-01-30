4b91563443824b5e80fca2d8dc71c79a3d9ea05d
openbao <14030756+openbao@user.noreply.gitee.com>
2024-01-30 16:17:12 +0000

    !6 优化首页样式 Merge pull request !6 from openbao/feat-new-home

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -13,25 +13,21 @@
     :style="{ marginTop: safeAreaInsets?.top + 'px' }"
   >
     <view class="mt-12">
-      <image src="/static/logo.svg" alt="" class="w-40 h-40 block mx-auto" />
+      <image src="/static/logo.svg" alt="" class="w-26 h-26 block mx-auto" />
     </view>
-    <view class="text-center text-4xl main-title-color mt-8">unibest</view>
+    <view class="text-center text-4xl main-title-color mt-3">unibest</view>
     <view class="text-center text-2xl mt-2 mb-8">最好用的 uniapp 开发模板</view>
-    <view class="text-justify max-w-100 m-auto">
-      unibest 是由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI 驱动的跨端快速启动模板，使用 VS Code
-      开发，具有代码提示、自动格式化、统一配置、代码片段等功能，同时内置了大量平时开发常用的基本组件，开箱即用，让你编写
-      uniapp 拥有 best 体验。</view
-    >
-    <view class="text-blue mt-8 text-center"> 详细示例 参考 hello-unibest 项目 </view>
+    <view class="text-justify max-w-100 m-auto text-4 indent mb-2">{{ description }} </view>
+    <view class="text-blue mt-8 text-left text-4"> 详细示例 参考 hello-unibest 项目 </view>
+
     <!-- #ifdef H5 -->
     <view class="my-2 text-center">
-      <a class="my-2 text-center" href="https://github.com/codercup/hello-unibest" target="_blank">
-        https://github.com/codercup/hello-unibest
-      </a>
+      <a class="my-2 text-center" :href="quoteUrl" target="_blank">{{ quoteUrl }}</a>
     </view>
     <!-- #endif -->
+
     <!-- #ifndef H5 -->
-    <view class="my-2 text-center">https://github.com/codercup/hello-unibest</view>
+    <view class="my-2 text-left text-4">{{ quoteUrl }}</view>
     <!-- #endif -->
   </view>
 </template>
@@ -41,6 +37,10 @@
 const { safeAreaInsets } = uni.getSystemInfoSync()
 
 const author = ref('菲鸽')
+const quoteUrl = ref('https://github.com/codercup/hello-unibest')
+const description = ref(
+  'unibest 是由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI 驱动的跨端快速启动模板，使用 VS Code 开发，具有代码提示、自动格式化、统一配置、代码片段等功能，同时内置了大量平时开发常用的基本组件，开箱即用，让你编写 uniapp 拥有 best 体验。',
+)
 
 onLoad(() => {
   console.log(author)
@@ -49,7 +49,6 @@ onLoad(() => {
 
 <style>
 .main-title-color {
-  /* color: #2c8d39; */
   color: #d14328;
 }
 </style>
