e12c719eca686c2c41f00a1a1c7b337b9b1170f7
菲鸽 <1020103647@qq.com>
2024-02-11 00:01:00 +0800

    feat: unibest 首页不加hello-unibest项目

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -19,16 +19,6 @@
     <view class="text-center text-2xl mt-2 mb-8">最好用的 uniapp 开发模板</view>
     <view class="text-justify max-w-100 m-auto text-4 indent mb-2">{{ description }} </view>
     <view class="text-blue mt-8 text-center text-sm"> 详细示例 参考 hello-unibest 项目 </view>
-
-    <!-- #ifdef H5 -->
-    <view class="my-2 text-center">
-      <a class="my-2 text-center" :href="quoteUrl" target="_blank">{{ quoteUrl }}</a>
-    </view>
-    <!-- #endif -->
-
-    <!-- #ifndef H5 -->
-    <view class="my-2 text-left text-sm">{{ quoteUrl }}</view>
-    <!-- #endif -->
   </view>
 </template>
 
@@ -37,7 +27,6 @@
 const { safeAreaInsets } = uni.getSystemInfoSync()
 
 const author = ref('菲鸽')
-const quoteUrl = ref('https://github.com/codercup/hello-unibest')
 const description = ref(
   'unibest 是由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI 驱动的跨端快速启动模板，使用 VS Code 开发，具有代码提示、自动格式化、统一配置、代码片段等功能，同时内置了大量平时开发常用的基本组件，开箱即用，让你编写 uniapp 拥有 best 体验。',
 )
