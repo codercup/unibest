d3dbd05f0ffe8552022c92800721a8eda9913fdc
菲鸽 <1020103647@qq.com>
2024-02-20 17:38:22 +0800

    feat: uv-ui demo

---
+++ src/pages/demo/base/uv-ui.vue
@@ -0,0 +1,22 @@
+<route lang="json5" type="page">
+{
+  style: { navigationBarTitleText: 'uv ui' },
+}
+</route>
+
+<template>
+  <view class="mt-10">
+    <uv-qrcode ref="qrcode" class="m-auto" size="300px" value="https://h5.uvui.cn"></uv-qrcode>
+  </view>
+</template>
+
+<script lang="ts" setup>
+console.log(uni.$uv.os())
+console.log(uni.$uv.sys())
+console.log(uni.$uv.trim(' abc ')) // 去除两端空格
+console.log(uni.$uv.random(1, 3)) // 获取随机数
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
