c093a46a0846422997c76f67fb0c21a7b7ed009b
菲鸽 <1020103647@qq.com>
2024-02-23 09:54:50 +0800

    feat: 引入 unocss-applet, 移除 @uni-helper/unocss-preset-uni

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,6 @@
 // uno.config.ts
 import {
+  Preset,
   defineConfig,
   presetAttributify,
   presetIcons,
@@ -7,14 +8,31 @@ import {
   transformerVariantGroup,
 } from 'unocss'
 
-import { presetUni } from '@uni-helper/unocss-preset-uni'
+import {
+  presetApplet,
+  presetRemRpx,
+  transformerApplet,
+  transformerAttributify,
+} from 'unocss-applet'
 
+const isH5 = process.env?.UNI_PLATFORM === 'h5'
+const isMp = process.env?.UNI_PLATFORM?.startsWith('mp') ?? false
+
+const presets: Preset[] = []
+if (!isMp) {
+  /**
+   * you can add `presetAttributify()` here to enable unocss attributify mode prompt
+   * although preset is not working for applet, but will generate useless css
+   * 为了不生产无用的css,要过滤掉 applet
+   */
+  // 支持css class属性化，eg: `<button bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600" text="sm white">attributify Button</button>`
+  presets.push(presetAttributify())
+}
 export default defineConfig({
   presets: [
-    // @ts-expect-error 类型兼容性
-    presetUni(),
-    // 支持css class属性化，eg: `<button bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600" text="sm white">attributify Button</button>`
-    presetAttributify(),
+    presetApplet({ enable: !isH5 }),
+    presetRemRpx(),
+    ...presets,
     // 支持图标，需要搭配图标库，eg: @iconify-json/carbon, 使用 `<button class="i-carbon-sun dark:i-carbon-moon" />`
     presetIcons({
       scale: 1.2,
@@ -36,5 +54,19 @@ export default defineConfig({
     // 启用 () 分组功能
     // 支持css class组合，eg: `<div class="hover:(bg-gray-400 font-medium) font-(light mono)">测试 unocss</div>`
     transformerVariantGroup(),
+    // Don't change the following order
+    transformerAttributify(),
+    transformerApplet(),
+  ],
+  rules: [
+    [
+      'p-safe',
+      {
+        padding:
+          'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)',
+      },
+    ],
+    ['pt-safe', { 'padding-top': 'env(safe-area-inset-top)' }],
+    ['pb-safe', { 'padding-bottom': 'env(safe-area-inset-bottom)' }],
   ],
 })
