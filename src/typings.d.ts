eb81b89a376b3205c8fbae20c4a2bfa14707d2f7
菲鸽 <1020103647@qq.com>
2024-03-01 14:27:45 +0800

    feat(demo): enum 使用范例

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // 全局要用的类型放到这里
 
 export type IResData<T> = {
@@ -12,4 +13,9 @@ export type IUserInfo = {
   /** 微信的 openid，非微信没有这个字段 */
   openid?: string
   token?: string
+}
+
+export enum TestEnum {
+  A = 'a',
+  B = 'b',
 }
