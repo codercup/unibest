d2b0f231dd1f0b03eb682b06daa45b8e900e3340
菲鸽 <1020103647@qq.com>
2024-03-03 19:31:47 +0800

    feat: rename.sh

---
+++ shell/rename.sh
@@ -0,0 +1,17 @@
+# pnpm build:app 之后会生成 unpackage/dist/build/app
+# 我要把它改名为 unpackage/dist/build/unibest_app_build
+
+# 只有存在新的 app 文件时，才执行这些操作！！否则会误删！！
+if test -d ./dist/build/app; then
+  echo '存在新打包出来的app'
+
+  cd ./dist/build/
+
+  # 1、删除旧的 rename 后的文件夹
+  if test -d './unibest_app_build'; then
+    rm -rf ./unibest_app_build
+  fi
+
+  # 2、把 app 命名为 unibest_app_build
+  mv ./app ./unibest_app_build
+fi
