9160540b555d0ca5bff043aacea865403d6e7426
菲鸽 <1020103647@qq.com>
2024-03-03 13:58:27 +0800

    fix: navbar 展示bug

--- src/pages/demo/base/navbar.vue
+++ src/pages/demo/base/navbar.vue
@@ -10,13 +10,19 @@
     left-icon="left"
     @clickLeft="goBack"
     class="fixed w-full"
+    :border="false"
+    :status-bar="true"
   ></uni-nav-bar>
-  <view class="h-11"></view>
-  <fly-content :line="20" />
+  <view class="h-11" :style="{ paddingTop: safeAreaInsets?.top + 'px' }"></view>
+  <view>
+    <fly-content :line="20" />
+  </view>
 </template>
 
 <script lang="ts" setup>
 const goBack = () => {
   uni.navigateBack()
 }
+// 获取屏幕边界到安全区域距离
+const { safeAreaInsets } = uni.getSystemInfoSync()
 </script>
