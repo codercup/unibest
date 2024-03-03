d870209544b1711b24bbc8aaffb342dceb00fdc6
菲鸽 <1020103647@qq.com>
2024-03-03 15:02:50 +0800

    refine: 自动导入

--- src/pages/demo/base/auto-import.vue
+++ src/pages/demo/base/auto-import.vue
@@ -11,5 +11,8 @@
     <AppTest> 这个组件会自动导入 </AppTest>
     <view class="h-10"></view>
     <AppTest2> 这个组件会自动导入 </AppTest2>
+    <view class="text-red-700 mt-4">
+      不可滥用自动导入，不是全局要用的组件不建议放到 `src/components` 里面，会增加包的体积
+    </view>
   </div>
 </template>
