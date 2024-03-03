470f34c689daa1efe6e1b0b2019e293b027f2cd7
菲鸽 <1020103647@qq.com>
2024-03-03 17:07:54 +0800

    feat: unibest title

--- pages.config.ts
+++ pages.config.ts
@@ -3,7 +3,7 @@ import { defineUniPages } from '@uni-helper/vite-plugin-uni-pages'
 export default defineUniPages({
   globalStyle: {
     navigationStyle: 'default',
-    navigationBarTitleText: 'vue3-uniapp',
+    navigationBarTitleText: 'unibest',
     navigationBarBackgroundColor: '#f8f8f8',
     navigationBarTextStyle: 'black',
     backgroundColor: '#FFFFFF',
