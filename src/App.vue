1ca2909dc5469c78920a52253e170b0c1516fa00
菲鸽 <1020103647@qq.com>
2024-03-04 20:47:50 +0800

    feat: add default css reset

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,49 @@ onHide(() => {
   console.log('App Hide')
 })
 </script>
+
+<style lang="scss">
+/* stylelint-disable selector-type-no-unknown */
+view,
+navigator,
+input,
+scroll-view {
+  box-sizing: border-box;
+}
+
+button::after {
+  border: none;
+}
+
+swiper,
+scroll-view {
+  flex: 1;
+  height: 100%;
+  overflow: hidden;
+}
+
+image {
+  width: 100%;
+  height: 100%;
+  vertical-align: middle;
+}
+
+// 单行省略使用 unocss: text-ellipsis
+// 两行省略
+.ellipsis-2 {
+  display: -webkit-box;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
+
+// 三行省略
+.ellipsis-3 {
+  display: -webkit-box;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+}
+</style>
