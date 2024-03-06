5acd2ff23197fb15c08e5be3e8f115cdd08e60e0
菲鸽 <1020103647@qq.com>
2024-03-06 09:17:31 +0800

    chore: 续上，去掉错误提示

--- src/pages/demo/base/uni-ui-icons.vue
+++ src/pages/demo/base/uni-ui-icons.vue
@@ -9,8 +9,5 @@
   <view class="m-4">
     <uni-icons type="contact" size="30"></uni-icons>
     <uni-icons type="contact" size="30" color="red"></uni-icons>
-    <view class="text-blue-300"
-      >注意在微信小程序中，不支持改颜色，即设置了颜色也会变成默认的#333, BUG</view
-    >
   </view>
 </template>
