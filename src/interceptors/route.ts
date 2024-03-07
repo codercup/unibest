c4cc2be00e41a9b8f58fcdc6a4ae71fb3ea64bcf
菲鸽 <1020103647@qq.com>
2024-03-07 19:00:25 +0800

    refactor: route.js

--- src/interceptors/route.ts
+++ src/interceptors/route.ts
@@ -5,7 +5,7 @@
  * 我这里应为大部分都可以随便进入，所以使用黑名单
  */
 import { useUserStore } from '@/store'
-import { getAllNeedLoginPages, allNeedLoginPages } from '@/utils'
+import { getNeedLoginPages, needLoginPages as _needLoginPages, isDev } from '@/utils'
 
 // TODO Check
 const loginRoute = '/pages/login/index'
@@ -14,17 +14,17 @@ const loginRoute = '/pages/login/index'
 // 官网数说可用 process.env 判断环境， 为啥我运行报错（小程序里）（h5正常）
 // console.log(process.env)
 
-// TODO: 通过环境区分，非硬编码
-const isDev = false
 // 黑名单登录拦截器 - （适用于大部分页面不需要登录，少部分页面需要登录）
 const navigateToInterceptor = {
   // 注意，这里的url是 '/' 开头的，如 '/pages/index/index'，跟 'pages.json' 里面的 path 不同
   invoke({ url }: { url: string }) {
     console.log(url)
-    let needLoginPages = allNeedLoginPages
+    let needLoginPages = []
     // 为了防止开发时出现BUG，这里每次都获取一下。生产环境可以移到函数外，性能更好
-    if (isDev) {
-      needLoginPages = getAllNeedLoginPages()
+    if (isDev()) {
+      needLoginPages = getNeedLoginPages()
+    } else {
+      needLoginPages = _needLoginPages
     }
     if (needLoginPages.includes(url)) {
       const userStore = useUserStore()
