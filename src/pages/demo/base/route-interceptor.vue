3fe6251227e037bbac9ef1bb472b3d56c5481d5f
菲鸽 <1020103647@qq.com>
2024-03-08 10:19:16 +0800

    refactor: 优化路由拦截页面样式

--- src/pages/demo/base/route-interceptor.vue
+++ src/pages/demo/base/route-interceptor.vue
@@ -6,12 +6,19 @@
 </route>
 
 <template>
-  <view class="">未登录不能来本页面</view>
-  <view class="">已登录才能来本页面</view>
+  <view class="mt-8 text-center">
+    <view class="leading-10">
+      用户是否已登录：<text>{{ !!userStore.userInfo.token }}</text>
+    </view>
+    <view class="text-gray">未登录不能来本页面</view>
+    <view class="text-gray">已登录才能来本页面</view>
+  </view>
 </template>
 
 <script lang="ts" setup>
-//
+import { useUserStore } from '@/store'
+
+const userStore = useUserStore()
 </script>
 
 <style lang="scss" scoped>
