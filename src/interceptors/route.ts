b5d9d97cb38a07bcb2ea4223897d615d66ab0cf5
菲鸽 <1020103647@qq.com>
2024-03-09 14:55:06 +0800

    refine: isLogined

--- src/interceptors/route.ts
+++ src/interceptors/route.ts
@@ -13,7 +13,7 @@ const loginRoute = '/pages/login/index'
 const isLogined = () => {
   const userStore = useUserStore()
   if (!userStore || !userStore.userInfo) return false
-  return userStore.userInfo?.token
+  return !!userStore.userInfo?.token
 }
 
 const isDev = import.meta.env.DEV
