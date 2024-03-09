b2c6046531a2a0c961789c17ac2c1d79d824ed81
菲鸽 <1020103647@qq.com>
2024-03-09 14:44:51 +0800

    feat: add uniUploadFile

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -41,4 +41,41 @@ export const http = <T>(options: CustomRequestOptions) => {
   })
 }
 
-export default http
+// uni.uploadFile封装
+export const uniFileUpload = <T>(options: CustomRequestOptions) => {
+  // 1. 返回 Promise 对象
+  return new Promise<IResData<T>>((resolve, reject) => {
+    uni.uploadFile({
+      ...options,
+      // 响应成功
+      success(res) {
+        // 状态码 2xx，参考 axios 的设计
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          // 文件上传接口的rea.data的类型为string，这里转一下
+          const resData = JSON.parse(res.data) as IResData<T>
+          resolve(resData)
+        } else if (res.statusCode === 401) {
+          // 401错误  -> 清理用户信息，跳转到登录页
+          // userStore.clearUserInfo()
+          // uni.navigateTo({ url: '/pages/login/login' })
+          reject(res)
+        } else {
+          // 其他错误 -> 根据后端错误信息轻提示
+          uni.showToast({
+            icon: 'none',
+            title: '文件上传错误',
+          })
+          reject(res)
+        }
+      },
+      // 响应失败
+      fail(err) {
+        uni.showToast({
+          icon: 'none',
+          title: '网络错误，换个网络试试',
+        })
+        reject(err)
+      },
+    })
+  })
+}
