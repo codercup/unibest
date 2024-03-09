367978416cf78564ae39c7efafa08723fec6db15
菲鸽 <1020103647@qq.com>
2024-03-09 15:02:29 +0800

    refactor: typings 不用导出

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,13 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 // 全局要用的类型放到这里
 
-export type IResData<T> = {
+type IResData<T> = {
   code: number
   msg: string
   result: T
 }
 
-export type IUserInfo = {
+type IUserInfo = {
   nickname?: string
   avatar?: string
   /** 微信的 openid，非微信没有这个字段 */
@@ -15,7 +16,7 @@ export type IUserInfo = {
   token?: string
 }
 
-export enum TestEnum {
+enum TestEnum {
   A = 'a',
   B = 'b',
 }
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-param-reassign */
 import qs from 'qs'
 import { useUserStore } from '@/store'
-import { IResData, IUserInfo } from '@/typings'
 
 type CustomRequestOptions = UniApp.RequestOptions & { query?: Record<string, any> }
 
