35ad3946ac39f109254e0fa51f8895ec355b829e
菲鸽 <1020103647@qq.com>
2024-03-09 14:55:57 +0800

    refactor: 路由拦截

--- src/interceptors/route.ts
+++ src/interceptors/route.ts
@@ -10,13 +10,20 @@ import { getNeedLoginPages, needLoginPages as _needLoginPages } from '@/utils'
 // TODO Check
 const loginRoute = '/pages/login/index'
 
+const isLogined = () => {
+  const userStore = useUserStore()
+  if (!userStore || !userStore.userInfo) return false
+  return !!userStore.userInfo?.token
+}
+
 const isDev = import.meta.env.DEV
 
 // 黑名单登录拦截器 - （适用于大部分页面不需要登录，少部分页面需要登录）
 const navigateToInterceptor = {
   // 注意，这里的url是 '/' 开头的，如 '/pages/index/index'，跟 'pages.json' 里面的 path 不同
   invoke({ url }: { url: string }) {
-    console.log(url)
+    console.log(url) // /pages/route-interceptor/index?name=feige&age=30
+    const path = url.split('?')[0]
     let needLoginPages: string[] = []
     // 为了防止开发时出现BUG，这里每次都获取一下。生产环境可以移到函数外，性能更好
     if (isDev) {
@@ -24,9 +31,10 @@ const navigateToInterceptor = {
     } else {
       needLoginPages = _needLoginPages
     }
-    if (needLoginPages.includes(url)) {
-      const userStore = useUserStore()
-      const isLogin = !!userStore.userInfo.token
+    console.log(needLoginPages.includes(path))
+
+    if (needLoginPages.includes(path)) {
+      const isLogin = isLogined()
       if (isLogin) {
         return true
       }
@@ -40,7 +48,8 @@ const navigateToInterceptor = {
 
 export const routeInterceptor = {
   install() {
-    // 拦截 request 请求
     uni.addInterceptor('navigateTo', navigateToInterceptor)
+    uni.addInterceptor('reLaunch', navigateToInterceptor)
+    uni.addInterceptor('redirectTo', navigateToInterceptor)
   },
 }
