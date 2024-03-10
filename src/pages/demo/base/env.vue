ad88952d486368041e84472b95ff08c4fe42d82e
菲鸽 <1020103647@qq.com>
2024-03-10 10:40:23 +0800

    refine: demo 代码

--- src/pages/demo/base/env.vue
+++ src/pages/demo/base/env.vue
@@ -5,7 +5,7 @@
 </route>
 
 <template>
-  <view class="mt-4 ml-4"> 当前环境 isDev：{{ isDev }} </view>
+  <view class="mt-4 ml-4"> 当前环境 isDev：{{ isDev }} (通过 import.meta.env.DEV 获取)</view>
 </template>
 
 <script lang="ts" setup>
