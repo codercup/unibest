ab39065296e3a16da9e3f94074659451e4994ed2
菲鸽 <1020103647@qq.com>
2024-03-10 10:49:44 +0800

    refine: demo 展示

--- src/pages/demo/base/uni-ui.vue
+++ src/pages/demo/base/uni-ui.vue
@@ -9,6 +9,6 @@
   <uni-card>
     <text>这是一个基础卡片示例，内容较少，此示例展示了一个没有任何属性不带阴影的卡片。</text>
   </uni-card>
-  <view>微信里面下面的 uni-badge 显示不出来，BUG</view>
   <uni-badge text="99"></uni-badge>
+  <uni-rate :value="3" />
 </template>
