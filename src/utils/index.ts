185c5df24df48062705834e86b41ccda8ff4e933
菲鸽 <1020103647@qq.com>
2024-03-18 09:27:50 +0800

    fix: getCurrentPages(...).at is not a function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,6 +8,13 @@ export const getIsTabbar = () => {
     return false
   }
   const pages = getCurrentPages()
-  const currPath = pages.at(-1).route
+  const lastPage = getArrElementByIdx(pages, -1)
+  const currPath = lastPage.route
   return !!pagesJson.tabBar.list.find((e) => e.pagePath === currPath)
+}
+
+export const getArrElementByIdx = (arr: any[], index: number) => {
+  if (index < 0) return arr[arr.length + index]
+  if (index >= arr.length) return undefined
+  return arr[index]
 }
