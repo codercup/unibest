842ebdd0caaca91d194ec9b6ad420b5151529983
菲鸽 <1020103647@qq.com>
2024-03-22 19:49:07 +0800

    fix: #28 uni-ui 使用

--- src/pages/demo/base/uni-ui.vue
+++ src/pages/demo/base/uni-ui.vue
@@ -11,4 +11,21 @@
   </uni-card>
   <uni-badge text="99"></uni-badge>
   <uni-rate :value="3" />
+  <view>
+    <button @click="open">打开弹窗</button>
+    <uni-popup ref="popup" type="bottom" border-radius="10px 10px 0 0"
+      >底部弹出 Popup 自定义圆角</uni-popup
+    >
+  </view>
 </template>
+
+<script>
+export default {
+  methods: {
+    open() {
+      // 通过组件定义的ref调用uni-popup方法 ,如果传入参数 ，type 属性将失效 ，仅支持 ['top','left','bottom','right','center']
+      this.$refs.popup.open('top')
+    },
+  },
+}
+</script>
