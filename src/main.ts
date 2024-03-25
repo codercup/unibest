8de552d83a85a6474c19cc66fffcf34232f66f18
菲鸽 <1020103647@qq.com>
2024-03-25 21:01:26 +0800

    feat: 移除 uv-ui, 改用 wot

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createSSRApp } from 'vue'
-import uvUI from '@climblee/uv-ui'
 import App from './App.vue'
 import store from './store'
 import i18n from './locale/index'
@@ -12,7 +11,6 @@ export function createApp() {
   const app = createSSRApp(App)
   app.use(store)
   app.use(i18n)
-  app.use(uvUI)
   app.use(routeInterceptor)
   app.use(requestInterceptor)
   return {
