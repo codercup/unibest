034684e62280f341f8a7e488401c5bff19717170
菲鸽 <1020103647@qq.com>
2024-03-28 17:39:01 +0800

    build: add log

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default ({ command, mode }) => {
   // console.log(mode === process.env.NODE_ENV) // true
 
   // mode: 区分生产环境还是开发环境
-  console.log(command, mode)
+  console.log('command, mode -> ', command, mode)
   // pnpm dev:h5 时得到 => serve development
   // pnpm build:h5 时得到 => build production
   // pnpm dev:mp-weixin 时得到 => build development (注意区别，command为build)
@@ -44,11 +44,13 @@ export default ({ command, mode }) => {
   // process.cwd(): 获取当前文件的目录跟地址
   // loadEnv(): 返回当前环境env文件中额外定义的变量
   const env = loadEnv(mode, path.resolve(process.cwd(), 'env'))
-  console.log(env)
-  console.log(process.env.UNI_PLATFORM) // 得到 mp-weixin, h5 等
+  console.log('env -> ', env)
+  console.log('process.env.UNI_PLATFORM: ', process.env.UNI_PLATFORM) // 得到 mp-weixin, h5, app 等
+  console.log('isH5: ', process.env.UNI_PLATFORM === 'h5') // 得到 mp-weixin, h5, app 等
 
   return defineConfig({
     envDir: './env', // 自定义env目录
+
     plugins: [
       UniPages({
         exclude: ['**/components/**/**.*', '**/my/**/**.vue'],
