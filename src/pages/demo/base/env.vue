e275543f45b01e0ba4af2618d306b834efaa98ef
菲鸽 <1020103647@qq.com>
2024-03-30 15:08:42 +0800

    fix: build 报错，不能在template里面写 import.meta.env

--- src/pages/demo/base/env.vue
+++ src/pages/demo/base/env.vue
@@ -9,21 +9,14 @@
     当前环境 import.meta.env.DEV:
     <text class="text-red-500 ml-4">{{ isDev }}</text>
   </view>
-  <!-- <view class="mt-4 ml-4">
-    当前环境 import.meta.env.: {{ JSON.stringify(env, null, 2) }} (通过 import.meta.env 获取)
-  </view> -->
   <view class="mt-4 ml-4">
-    当前环境所使用的环境文件 import.meta.env.VITE_ENV_FILE:
-    <text class="text-red-500 ml-4">{{ env.VITE_ENV_FILE }}</text>
+    当前环境所使用的环境文件
+    <text class="text-red-500 ml-4">{{ VITE_ENV_FILE }}</text>
   </view>
   <view class="text-red-500 mt-8 ml-4">环境变量的使用与 --mode 参数配合使用</view>
 </template>
 
 <script lang="ts" setup>
 const isDev = import.meta.env.DEV
-const { env } = import.meta
+const { VITE_ENV_FILE } = import.meta.env
 </script>
-
-<style lang="scss" scoped>
-//
-</style>
