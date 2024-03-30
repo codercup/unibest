c32cc540e95b89c331fbd4d9d6dcf3e212cec4bd
菲鸽 <1020103647@qq.com>
2024-03-30 11:09:25 +0800

    feat: 引入 vite-plugin-mock

---
+++ src/mockProdServer.ts
@@ -0,0 +1,18 @@
+import { createProdMockServer } from 'vite-plugin-mock/dist/client'
+
+const modules = import.meta.glob('../mock/**/*.ts', { eager: true })
+
+const mockModules: any[] = []
+Object.keys(modules).forEach((key) => {
+  if (key.includes('/_')) {
+    return
+  }
+  mockModules.push(...(modules as Recordable)[key].default)
+})
+
+/**
+ * Used in a production environment. Need to manually import all modules
+ */
+export function setupProdMockServer() {
+  createProdMockServer(mockModules)
+}
---
+++ src/pages/demo/base/mock.vue
@@ -0,0 +1,45 @@
+<route lang="json5" type="page">
+{
+  layout: 'demo',
+  style: {
+    navigationBarTitleText: 'mock',
+  },
+}
+</route>
+
+<template>
+  <view class="mt-6">
+    <button @click="getFoo" class="my-4" type="primary">测试 GET 请求</button>
+    <view class="text-xl">请求数据如下</view>
+    <view class="text-green h-10">{{ JSON.stringify(data) }}</view>
+    <view class="text-xl">完整数据</view>
+    <view class="text-green h-20">{{ JSON.stringify(originalData) }}</view>
+    <button class="my-8" type="warn" @click="reset">一键清空数据</button>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { getMockAPI } from '@/service/mock'
+
+onLoad(() => {
+  getFoo()
+})
+
+const originalData = ref<IResData<any>>()
+const data = ref()
+
+const getFoo = async () => {
+  const res = await getMockAPI('菲鸽')
+  data.value = res.result
+  originalData.value = res
+}
+
+const reset = () => {
+  data.value = undefined
+  originalData.value = undefined
+}
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
