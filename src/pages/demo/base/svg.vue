9e66c36bb3282b872e62b1cc341d7b6bba25a359
菲鸽 <1020103647@qq.com>
2024-04-05 19:13:54 +0800

    feat: svg 的使用

---
+++ src/pages/demo/base/svg.vue
@@ -0,0 +1,33 @@
+<route lang="json5" type="page">
+{
+  layout: 'default',
+  style: {
+    navigationBarTitleText: 'svg使用',
+  },
+}
+</route>
+
+<template>
+  <view class="p-4">
+    <view class="text-center m-4">svg 多种使用方法</view>
+    <image src="/static/svg/task.svg" mode="scaleToFill" class="h-20 w-20" />
+    <image :src="iconUrl" mode="scaleToFill" class="h-20 w-20" />
+    <!-- #ifdef H5 -->
+    <TaskComponent class="h-20 w-20" />
+    <!-- #endif -->
+  </view>
+</template>
+
+<script lang="ts" setup>
+// eslint-disable-next-line import/no-duplicates
+import iconUrl from './task.svg?url'
+// #ifdef H5
+// 小程序不支持 compnent 方式引入，运行会报错
+// eslint-disable-next-line import/no-duplicates
+import TaskComponent from './task.svg?component'
+// #endif
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
