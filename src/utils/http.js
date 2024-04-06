5f1465a32fb549906107fa93b225e7cca22bfb99
菲鸽 <1020103647@qq.com>
2024-04-06 21:36:28 +0800

    fix: http file error

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,47 +1,42 @@
 export const http = (options) => {
   // 1. 返回 Promise 对象
-  return (
-    new Promise() <
-    IResData <
-    T >>
-      ((resolve, reject) => {
-        uni.request({
-          ...options,
-          dataType: 'json',
-          // #ifndef MP-WEIXIN
-          responseType: 'json',
-          // #endif
-          // 响应成功
-          success(res) {
-            // 状态码 2xx，参考 axios 的设计
-            if (res.statusCode >= 200 && res.statusCode < 300) {
-              // 2.1 提取核心数据 res.data
-              resolve(res.data)
-            } else if (res.statusCode === 401) {
-              // 401错误  -> 清理用户信息，跳转到登录页
-              // userStore.clearUserInfo()
-              // uni.navigateTo({ url: '/pages/login/login' })
-              reject(res)
-            } else {
-              // 其他错误 -> 根据后端错误信息轻提示
-              uni.showToast({
-                icon: 'none',
-                title: res.data.msg || '请求错误',
-              })
-              reject(res)
-            }
-          },
-          // 响应失败
-          fail(err) {
-            uni.showToast({
-              icon: 'none',
-              title: '网络错误，换个网络试试',
-            })
-            reject(err)
-          },
+  return new Promise((resolve, reject) => {
+    uni.request({
+      ...options,
+      dataType: 'json',
+      // #ifndef MP-WEIXIN
+      responseType: 'json',
+      // #endif
+      // 响应成功
+      success(res) {
+        // 状态码 2xx，参考 axios 的设计
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          // 2.1 提取核心数据 res.data
+          resolve(res.data)
+        } else if (res.statusCode === 401) {
+          // 401错误  -> 清理用户信息，跳转到登录页
+          // userStore.clearUserInfo()
+          // uni.navigateTo({ url: '/pages/login/login' })
+          reject(res)
+        } else {
+          // 其他错误 -> 根据后端错误信息轻提示
+          uni.showToast({
+            icon: 'none',
+            title: res.data.msg || '请求错误',
+          })
+          reject(res)
+        }
+      },
+      // 响应失败
+      fail(err) {
+        uni.showToast({
+          icon: 'none',
+          title: '网络错误，换个网络试试',
         })
-      })
-  )
+        reject(err)
+      },
+    })
+  })
 }
 
 // uni.uploadFile封装
