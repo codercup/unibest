6a2aea39686a0f8a8b3217d69b2df098eca43dbc
菲鸽 <1020103647@qq.com>
2024-04-14 18:24:42 +0800

    refine: svg

--- src/pages/demo/base/svg.vue
+++ src/pages/demo/base/svg.vue
@@ -12,12 +12,10 @@
     <view class="text-center m-4">svg 多种使用方法</view>
     <image src="/static/svg/task.svg" mode="scaleToFill" class="h-20 w-20" />
     <image :src="iconUrl" mode="scaleToFill" class="h-20 w-20" />
-    <view class="text-center m-4 text-blue-500">目前发现小程序只支持 image 方式</view>
-    <view class="text-center m-4 text-blue-500">下面的几种方式，小程序和APP都不支持</view>
+    <view class="text-center m-4 text-blue-500">目前发现，小程序和APP只支持 image 方式</view>
+    <view class="text-center m-4 text-blue-500">下面的几种方式，都不支持</view>
     <!-- #ifdef H5 -->
-    <!-- 小程序和APP 不支持 component 方式引入，运行会报错 -->
     <TaskComponent class="h-20 w-20" />
-    <!-- SvgIcon 的方式小程序和APP也不支持 -->
     <SvgIcon name="task" class="h-20 w-20"></SvgIcon>
     <SvgIcon name="dir-task2" class="h-20 w-20"></SvgIcon>
     <!-- #endif -->
@@ -28,7 +26,7 @@
 // eslint-disable-next-line import/no-duplicates
 import iconUrl from './task.svg?url'
 // #ifdef H5
-// 小程序不支持 component 方式引入，运行会报错
+// 小程序不支持 component 方式引入，运行会报错，APP不报错，但是不生效
 // eslint-disable-next-line import/no-duplicates
 import TaskComponent from './task.svg?component'
 // #endif
