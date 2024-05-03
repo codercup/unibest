6f29aa2f437d902361887727f8176f6b071ad182
菲鸽 <1020103647@qq.com>
2024-05-03 23:16:42 +0800

    feat: 经过测试 defineModel 在h5 是OK的，其他端不行

--- src/pages/index/about.vue
+++ src/pages/index/about.vue
@@ -26,10 +26,17 @@
       <wd-button type="primary" @click="gotoPage('upload')">上传demo</wd-button>
       <wd-button type="primary" @click="gotoPage('upload2')">上传demo2(请求状态一体化)</wd-button>
     </view>
+
+    <view class="mt-8">vue 升级到了 3.4+，测试下 defineModel 是否可用</view>
+
+    <!-- Parent.vue -->
+    <view class="mt-8">countModel: {{ countModel }}</view>
+    <Child v-model="countModel" />
   </view>
 </template>
 
 <script lang="ts" setup>
+import Child from './components/Child.vue'
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 
@@ -38,6 +45,8 @@ const gotoPage = (page: string) => {
     url: `/pages/index/${page}`,
   })
 }
+
+const countModel = ref(0)
 </script>
 
 <style lang="scss" scoped>
---
+++ src/pages/index/components/Child.vue
@@ -0,0 +1,22 @@
+<route lang="json5" type="page">
+{
+  layout: 'default',
+  style: {
+    navigationBarTitleText: '',
+  },
+}
+</route>
+
+<!-- Child.vue -->
+<script setup>
+const model = defineModel()
+
+function update() {
+  model.value++
+}
+</script>
+
+<template>
+  <div>parent bound v-model is: {{ model }}</div>
+  <div><button @click="update">update</button></div>
+</template>
