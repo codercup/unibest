f42dcea4bcf2afcd2d1dd17e321b5e516c4d8ed6
菲鸽 <1020103647@qq.com>
2024-05-12 16:55:31 +0800

    chore: 按钮文案优化，精简

--- src/pages/index/about.vue
+++ src/pages/index/about.vue
@@ -19,13 +19,15 @@
     <view class="text-center mt-8 text-#fff">
       <wd-button type="primary" @click="gotoPage('request')">去请求页</wd-button>
       <wd-button type="primary" @click="gotoPage('request2')" class="ml-2">
-        去请求页2 (请求状态一体化)
+        去请求页2 (状态一体)
       </wd-button>
     </view>
 
     <view class="text-center mt-8 text-#fff">
       <wd-button type="primary" @click="gotoPage('upload')">上传demo</wd-button>
-      <wd-button type="primary" @click="gotoPage('upload2')">上传demo2(请求状态一体化)</wd-button>
+      <wd-button type="primary" @click="gotoPage('upload2')" class="ml-2">
+        上传demo2(状态一体)
+      </wd-button>
     </view>
     <view class="text-center mt-8">
       <text mr-2>iconfont:</text>
