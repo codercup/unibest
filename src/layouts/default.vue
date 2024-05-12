c51391570f2eaa3fb2c352cdc6795c30dc2afd0f
菲鸽 <1020103647@qq.com>
2024-05-12 16:24:37 +0800

    feat: 多语言

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,17 @@
 <template>
-  <wd-config-provider>
+  <wd-config-provider :themeVars="themeVars">
     <slot />
     <wd-toast />
     <wd-message-box />
   </wd-config-provider>
 </template>
+
+<script lang="ts" setup>
+import type { ConfigProviderThemeVars } from 'wot-design-uni'
+
+const themeVars: ConfigProviderThemeVars = {
+  // colorTheme: 'red',
+  // buttonPrimaryBgColor: '#07c160',
+  // buttonPrimaryColor: '#07c160',
+}
+</script>
