c4f954bca72f59416fc04ffc56372b388592daf4
菲鸽 <1020103647@qq.com>
2024-05-12 15:30:57 +0800

    feat: 引入z-paging

--- pages.config.ts
+++ pages.config.ts
@@ -12,6 +12,8 @@ export default defineUniPages({
     autoscan: true,
     custom: {
       '^wd-(.*)': 'wot-design-uni/components/wd-$1/wd-$1.vue',
+      '^(?!z-paging-refresh|z-paging-load-more)z-paging(.*)':
+        'z-paging/components/z-paging$1/z-paging$1.vue',
     },
   },
   tabBar: {
