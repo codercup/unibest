c5433b137231fc9594c850de54508c29d7b6760b
Jinliang <miku.cy@foxmail.com>
2024-06-08 10:40:40 +0800

    feat: 增加 ensureImmediate 选项以避免生命周期钩子错误
    - 新增 `ensureImmediate` 选项，确保在钩子调用时立即执行请求。
    - 如果 `ensureImmediate` 为 `true`，在钩子调用时立即执行 `run` 函数。
    - 保持 `immediate` 选项在 `onLoad` 中执行请求。
    
    这些更改可以避免在生命周期钩子中调用 `onLoad` 时出现的错误，确保请求能够在钩子调用时立即执行。
    
    使用示例：
    
    const { run } = useRequest(fetchData, { ensureImmediate: true })
    
    try {
      const result = await run()
      console.log('成功:', result)
    } catch (err) {
      console.error('失败:', err)
    }

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -3,6 +3,8 @@ import { UnwrapRef } from 'vue'
 type IUseRequestOptions<T> = {
   /** 是否立即执行，如果是则在onLoad执行 */
   immediate?: boolean
+  /** 是否保证立即执行，如果是则在钩子调用时立即执行 */
+  ensureImmediate?: boolean
   /** 初始化数据 */
   initialData?: T
 }
@@ -11,13 +13,34 @@ type IUseRequestOptions<T> = {
  * useRequest是一个定制化的请求钩子，用于处理异步请求和响应。
  * @param func 一个执行异步请求的函数，返回一个包含响应数据的Promise。
  * @param options 包含请求选项的对象 {immediate, initialData}。
- * @param options.immediate 是否立即执行请求，默认为true。
+ * @param options.immediate 是否立即执行，如果是则在onLoad执行，默认为true。
+ * @param options.ensureImmediate 是否保证立即执行，如果是则在钩子调用时立即执行，默认为false。
  * @param options.initialData 初始化数据，默认为undefined。
  * @returns 返回一个对象{loading, error, data, run}，包含请求的加载状态、错误信息、响应数据和手动触发请求的函数。
+ * @example
+ * 1. 默认在组件挂载后立即执行请求：
+ *
+ * const { loading, error, data, run } = useRequest(() => apiCall());
+ *
+ * 2. 钩子调用时立即执行执行请求：
+ *
+ * const { loading, error, data, run } = useRequest(() => apiCall(), { ensureImmediate: true });
+ *
+ * 3. 不在组件挂载后立即执行请求，需要手动触发：
+ *
+ * const { loading, error, data, run } = useRequest(() => apiCall(), { immediate: false });
+ * onMounted(() => {
+ *   run();
+ * });
+ *
+ * 注意事项：
+ * - `immediate` 参数控制请求是否在组件挂载后立即执行。适用于需要在组件挂载后立即执行请求的场景。
+ * - `ensureImmediate` 参数强制请求在组件挂载后立即执行，不管 `immediate` 的值。这在需要无条件立即执行请求的场景中非常有用。
+ * - 当同时设置 `immediate: false` 和 `ensureImmediate: true` 时，请求仍会立即执行。
  */
 export default function useRequest<T>(
   func: () => Promise<IResData<T>>,
-  options: IUseRequestOptions<T> = { immediate: true },
+  options: IUseRequestOptions<T> = { immediate: true, ensureImmediate: false },
 ) {
   const loading = ref(false)
   const error = ref(false)
@@ -39,8 +62,12 @@ export default function useRequest<T>(
       })
   }
 
-  onLoad(() => {
-    options.immediate && run()
-  })
+  if (options.ensureImmediate) {
+    // 钩子调用时立即执行执行请求
+    run()
+  } else if (options.immediate) {
+    // 组件挂载后立即执行请求
+    onLoad(() => run())
+  }
   return { loading, error, data, run }
 }
