b3a723a7683192ef0d0990f2855f77d136bdfe92
Jinliang <miku.cy@foxmail.com>
2024-06-08 15:35:28 +0800

    Revert "feat: 增加 ensureImmediate 选项以避免生命周期钩子错误"
    This reverts commit c5433b137231fc9594c850de54508c29d7b6760b.

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -3,8 +3,6 @@ import { UnwrapRef } from 'vue'
 type IUseRequestOptions<T> = {
   /** 是否立即执行，如果是则在onLoad执行 */
   immediate?: boolean
-  /** 是否保证立即执行，如果是则在钩子调用时立即执行 */
-  ensureImmediate?: boolean
   /** 初始化数据 */
   initialData?: T
 }
@@ -12,35 +10,14 @@ type IUseRequestOptions<T> = {
 /**
  * useRequest是一个定制化的请求钩子，用于处理异步请求和响应。
  * @param func 一个执行异步请求的函数，返回一个包含响应数据的Promise。
- * @param options 包含请求选项的对象 {immediate, ensureImmediate， initialData}。
- * @param options.immediate 是否立即执行，如果是则在onLoad执行，默认为true。
- * @param options.ensureImmediate 是否保证立即执行，如果是则在钩子调用时立即执行，默认为false。
+ * @param options 包含请求选项的对象 {immediate, initialData}。
+ * @param options.immediate 是否立即执行请求，默认为true。
  * @param options.initialData 初始化数据，默认为undefined。
  * @returns 返回一个对象{loading, error, data, run}，包含请求的加载状态、错误信息、响应数据和手动触发请求的函数。
- * @example
- * 1. 默认在组件挂载后立即执行请求：
- *
- * const { loading, error, data, run } = useRequest(() => apiCall());
- *
- * 2. 钩子调用时立即执行执行请求：
- *
- * const { loading, error, data, run } = useRequest(() => apiCall(), { ensureImmediate: true });
- *
- * 3. 不在组件挂载后立即执行请求，需要手动触发：
- *
- * const { loading, error, data, run } = useRequest(() => apiCall(), { immediate: false });
- * onMounted(() => {
- *   run();
- * });
- *
- * 注意事项：
- * - `immediate` 参数控制请求是否在组件挂载后立即执行。适用于需要在组件挂载后立即执行请求的场景。
- * - `ensureImmediate` 参数确保请求在钩子调用时立即执行，不管 `immediate` 的值。适合无条件立即执行请求的场景。
- * - 当同时设置 `immediate: false` 和 `ensureImmediate: true` 时，请求仍会立即执行。
  */
 export default function useRequest<T>(
   func: () => Promise<IResData<T>>,
-  options: IUseRequestOptions<T> = { immediate: true, ensureImmediate: false },
+  options: IUseRequestOptions<T> = { immediate: true },
 ) {
   const loading = ref(false)
   const error = ref(false)
@@ -62,12 +39,8 @@ export default function useRequest<T>(
       })
   }
 
-  if (options.ensureImmediate) {
-    // 钩子调用时立即执行执行请求
-    run()
-  } else if (options.immediate) {
-    // 组件挂载后立即执行请求
-    onLoad(() => run())
-  }
+  onLoad(() => {
+    options.immediate && run()
+  })
   return { loading, error, data, run }
 }
