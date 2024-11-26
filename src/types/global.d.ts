6b99490232e9a88cd96336286cb541a00fbdfe20
gladtoeatu <w.h@miigu.com>
2024-11-26 13:07:53 +0800

    修复block、template在IntrinsicElements未定义的提示
    修复block、template在IntrinsicElements未定义的提示

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -14,3 +14,10 @@ declare const __UNI_PLATFORM__:
   | 'quickapp-webview-union'
 
 declare const __VITE_APP_PROXY__: 'true' | 'false'
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    template: any
+    block: any
+  }
+}
