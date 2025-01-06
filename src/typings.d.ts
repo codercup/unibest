466724080e4360d6f86c20db84592bc3e92b129b
菲鸽 <1020103647@qq.com>
2025-01-06 13:32:39 +0800

    Merge pull request #42 from Rem-Blove/main
    fix: 枚举失效问题 & 分离全局声明和模块化内容

---
+++ src/typings.d.ts
@@ -0,0 +1,28 @@
+// 全局要用的类型放到这里
+
+declare global {
+  type IResData<T> = {
+    code: number
+    msg: string
+    data: T
+  }
+
+  // uni.uploadFile文件上传参数
+  type IUniUploadFileOptions = {
+    file?: File
+    files?: UniApp.UploadFileOptionFiles[]
+    filePath?: string
+    name?: string
+    formData?: any
+  }
+
+  type IUserInfo = {
+    nickname?: string
+    avatar?: string
+    /** 微信的 openid，非微信没有这个字段 */
+    openid?: string
+    token?: string
+  }
+}
+
+export {} // 防止模块污染
