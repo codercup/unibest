32820c554376eff71453effc7810571e7772a54a
Burt <burt.lai@kupotech.com>
2025-01-08 21:20:22 +0800

    fix: 测试 standard-version: fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onLaunch, onShow, onHide } from '@dcloudio/uni-app'
+import { onHide, onLaunch, onShow } from '@dcloudio/uni-app'
 
 onLaunch(() => {
   console.log('App Launch')
