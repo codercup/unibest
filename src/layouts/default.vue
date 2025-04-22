fd64936e36843503dbb15eba84797759cc69ec08
laifeipeng <1020103647@qq.com>
2025-04-22 14:44:16 +0800

    feat: 去掉 tabbar

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,7 +7,14 @@
 </template>
 
 <script lang="ts" setup>
+import { getIsTabbar } from '@/utils'
 import type { ConfigProviderThemeVars } from 'wot-design-uni'
+
+onMounted(() => {
+  if (getIsTabbar()) {
+    uni.hideTabBar()
+  }
+})
 
 const themeVars: ConfigProviderThemeVars = {
   // colorTheme: 'red',
