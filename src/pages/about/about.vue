49fa8c45097735d6980fbfc32729b039877d0082
feige996 <1020102647@qq.com>
2025-05-26 21:09:47 +0800

    feat(about): 添加自定义导航栏组件并配置自动导入
    在关于页面添加自定义导航栏组件 fg-navbar，并配置 pages.config.ts 实现自动导入
    新增 fg-navbar 组件，基于 wd-navbar 封装，提供统一的导航栏样式和返回逻辑
    ```
    
    这个提交消息：
    1. 使用 feat 类型，因为是新增功能
    2. 添加了 scope (about) 表示主要修改的是关于页面
    3. 描述部分简洁说明了"添加自定义导航栏组件"这个主要变更和"配置自动导入"这个配套修改
    4. 在正文中补充说明了新增组件的实现细节和目的

--- src/pages/about/about.vue
+++ src/pages/about/about.vue
@@ -11,6 +11,7 @@
     class="bg-white overflow-hidden pt-2 px-4"
     :style="{ marginTop: safeAreaInsets?.top + 'px' }"
   >
+    <fg-navbar>关于</fg-navbar>
     <view class="text-center text-3xl mt-8">
       鸽友们好，我是
       <text class="text-red-500">菲鸽</text>
