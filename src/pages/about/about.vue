e2c319079a19dc9ecddac05b6a0712d2138cdba7
feige996 <1020102647@qq.com>
2025-06-21 15:30:53 +0800

    refactor(about): 移除未使用的导航代码并简化页面结构

--- src/pages/about/about.vue
+++ src/pages/about/about.vue
@@ -9,19 +9,12 @@
 
 <template>
   <view>
-    <view
-      class="bg-white overflow-hidden pt-2 px-4"
-      :style="{ marginTop: safeAreaInsets?.top + 'px' }"
-    >
-      <view class="text-center text-3xl mt-8">
-        鸽友们好，我是
-        <text class="text-red-500">菲鸽</text>
-      </view>
-      <!-- <button @click="toSubPage()">去分包</button> -->
-      <view class="test-css">测试 scss 样式</view>
-      <RequestComp />
-      <UploadComp />
+    <view class="text-center text-3xl mt-8">
+      鸽友们好，我是
+      <text class="text-red-500">菲鸽</text>
     </view>
+    <RequestComp />
+    <UploadComp />
   </view>
 </template>
 
@@ -31,11 +24,6 @@ import UploadComp from './components/upload.vue'
 
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
-const toSubPage = () => {
-  uni.navigateTo({
-    url: '/pages-sub/demo/index',
-  })
-}
 
 // 奇怪：同样的代码放在 vue 里面不会校验到错误，放在 .ts 文件里面会校验到错误
 // const testOxlint = (name: string) => {
