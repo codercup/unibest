96abac9c21201e0423478f6c63493e545279e775
feige996 <1020102647@qq.com>
2025-08-05 17:52:54 +0800

    feat(service): 添加PetStore API服务层实现
    实现PetStore API的所有服务接口，包括用户、宠物和商店相关操作
    添加类型定义、基础API请求和Vue Query封装
    配置openapi-ts-request生成器路径

--- openapi-ts-request.config.ts
+++ openapi-ts-request.config.ts
@@ -3,7 +3,7 @@ import type { GenerateServiceProps } from 'openapi-ts-request'
 export default [
   {
     schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
-    serversPath: './src/service/app',
+    serversPath: './src/service',
     requestLibPath: `import request from '@/http/vue-query';\n import { CustomRequestOptions } from '@/http/interceptor';`,
     requestOptionsType: 'CustomRequestOptions',
     isGenReactQuery: true,
---
+++ src/service/index.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// @ts-ignore
+export * from './types';
+export * from './displayEnumLabel';
+
+export * from './pet';
+export * from './pet.vuequery';
+export * from './store';
+export * from './store.vuequery';
+export * from './user';
+export * from './user.vuequery';
