37021672b307dc98051060700573c617a1a0012e
feige996 <1020102647@qq.com>
2025-08-07 21:13:44 +0800

    chore(about): 移除未使用的recommendUrl变量并整理代码格式

--- src/pages/about/components/request.vue
+++ src/pages/about/components/request.vue
@@ -2,13 +2,12 @@
 import type { IFooItem } from '@/api/foo'
 import { getFooAPI } from '@/api/foo'
 
-const recommendUrl = ref('http://laf.run/signup?code=ohaOgIX')
-
 // const initialData = {
 //   name: 'initialData',
 //   id: '1234',
 // }
 const initialData = undefined
+
 const { loading, error, data, run } = useRequest<IFooItem>(() => getFooAPI('菲鸽'), {
   immediate: true,
   initialData,
