5f442e0ed9d0bbc004826cc59d9b6c730c50537f
feige996 <1020102647@qq.com>
2025-08-15 09:57:18 +0800

    fix(h5): 设置路由基础路径为根目录
    将h5路由的基础路径配置为根目录，确保部署时路径正确。同时在manifest.json和manifest.config.ts中同步此配置，并更新相关文档链接。

--- manifest.config.ts
+++ manifest.config.ts
@@ -30,7 +30,7 @@ export default defineManifestConfig({
   'locale': VITE_FALLBACK_LOCALE, // 'zh-Hans'
   'h5': {
     router: {
-      // base: VITE_APP_PUBLIC_BASE,
+      base: VITE_APP_PUBLIC_BASE,
     },
   },
   /* 5+App特有相关 */
