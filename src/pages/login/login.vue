a9b00871315e15a8c67545c13f79b4f46d640ef9
feige996 <1020102647@qq.com>
2025-08-18 19:53:57 +0800

    feat(login): 添加多平台登录页面组件
    添加H5、微信小程序和通用登录页面组件，通过条件编译实现多平台适配

---
+++ src/pages/login/login.vue
@@ -0,0 +1,26 @@
+<route lang="jsonc" type="page">
+{
+  "layout": "default",
+  "style": {
+    "navigationBarTitleText": "登录"
+  }
+}
+</route>
+
+<script lang="ts" setup>
+import LoginH5 from './login.h5.vue'
+import LoginWeixin from './login.weixin.vue'
+</script>
+
+<template>
+  <!-- #ifdef H5 || APP-PLUS -->
+  <LoginH5 />
+  <!-- #endif -->
+  <!-- #ifdef MP-WEIXIN -->
+  <LoginWeixin />
+  <!-- #endif -->
+</template>
+
+<style lang="scss" scoped>
+//
+</style>
---
+++ src/pages/login/login.weixin.vue
@@ -0,0 +1,26 @@
+<route lang="jsonc" type="page">
+{
+  "layout": "default",
+  "style": {
+    "navigationBarTitleText": "登录"
+  }
+}
+</route>
+
+<script lang="ts" setup>
+//
+</script>
+
+<template>
+  <view class="login">
+    <view class="login__header">
+      <view class="login__header__title">
+        登录 mp-weixin
+      </view>
+    </view>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+//
+</style>
