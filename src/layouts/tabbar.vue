d361916fb6dca50a155511ba0f7d14db9b81fd59
feige996 <1020102647@qq.com>
2025-08-19 18:19:12 +0800

    refactor(layouts): 移除未使用的主题变量声明
    清理默认布局和标签栏布局中已注释掉的主题变量配置

--- src/layouts/tabbar.vue
+++ src/layouts/tabbar.vue
@@ -1,15 +1,9 @@
 <script lang="ts" setup>
-import type { ConfigProviderThemeVars } from 'wot-design-uni'
 import { useThemeStore } from '@/store'
 import { getI18nText } from '@/tabbar/i18n'
 import FgTabbar from '@/tabbar/index.vue'
 import { getCurrentPageI18nKey } from '@/utils'
 
-const themeVars: ConfigProviderThemeVars = {
-  // colorTheme: 'red',
-  // buttonPrimaryBgColor: '#07c160',
-  // buttonPrimaryColor: '#07c160',
-}
 onShow(() => {
   console.log('layout tabbar - onSHow')
   uni.setNavigationBarTitle({
