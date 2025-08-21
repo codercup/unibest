47b0d4941545431b48ff0e3afd8785167e34ba6a
feige996 <1020102647@qq.com>
2025-08-21 15:20:20 +0800

    refactor(pages.json): 移除未使用的图标配置

--- src/tabbar/config.ts
+++ src/tabbar/config.ts
@@ -20,7 +20,7 @@ export const TABBAR_STRATEGY_MAP = {
 // 如果是使用 NO_TABBAR(0)，nativeTabbarList 和 customTabbarList 都不生效(里面的配置不用管)
 // 如果是使用 NATIVE_TABBAR(1)，只需要配置 nativeTabbarList，customTabbarList 不生效
 // 如果是使用 CUSTOM_TABBAR(2,3)，只需要配置 customTabbarList，nativeTabbarList 不生效
-export const selectedTabbarStrategy = TABBAR_STRATEGY_MAP.CUSTOM_TABBAR_WITH_CACHE
+export const selectedTabbarStrategy = TABBAR_STRATEGY_MAP.NATIVE_TABBAR
 
 type NativeTabBarItem = TabBar['list'][number]
 
