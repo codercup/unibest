d77fb715b76ac4f999673e39a92c359cae479f6f
feige996 <1020102647@qq.com>
2025-09-07 16:52:13 +0800

    chore(index): 移除首页登录相关代码

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { LOGIN_PAGE } from '@/router/config'
 import { useThemeStore } from '@/store'
 import { safeAreaInsets } from '@/utils/systemInfo'
 
@@ -18,7 +17,6 @@ definePage({
 
 const themeStore = useThemeStore()
 
-const author = ref('菲鸽')
 const description = ref(
   'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite5 + UnoCss + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
 )
@@ -27,12 +25,6 @@ console.log('index/index 首页打印了')
 onLoad(() => {
   console.log('测试 uni API 自动引入: onLoad')
 })
-
-function toLogin() {
-  uni.navigateTo({
-    url: LOGIN_PAGE,
-  })
-}
 </script>
 
 <template>
@@ -104,9 +96,6 @@ function toLogin() {
         https://wot-design-uni.cn
       </text>
     </view>
-    <button class="mt-4 w-40 text-center" @click="toLogin">
-      点击去登录页
-    </button>
     <view class="h-6" />
   </view>
 </template>
