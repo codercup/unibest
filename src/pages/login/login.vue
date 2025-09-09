4a88ca56b92505ed207f47fd5130a3e3a9ed8ae9
feige996 <1020102647@qq.com>
2025-09-09 21:20:41 +0800

    refactor(登录): 调整登录流程中token和用户信息的设置顺序
    将token设置移到用户信息设置之前，以更符合常见登录流程

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -32,7 +32,15 @@ async function doLogin() {
     return
   }
   try {
-    // 1/2 调用接口回来后设置用户信息
+    // 有的时候后端会用一个接口返回token和用户信息，有的时候会分开2个接口（各有利弊，看业务场景和系统复杂度），这里使用2个接口返回的来模拟
+    // 1/2 调用接口回来后设置token信息
+    // 这里用单token来模拟
+    tokenStore.setTokenInfo({
+      token: '123456',
+      expiresIn: 60 * 60 * 24 * 7,
+    })
+
+    // 2/2 调用接口回来后设置用户信息
     // const res = await login({
     //   username: '菲鸽',
     //   password: '123456',
@@ -44,12 +52,7 @@ async function doLogin() {
       nickname: '菲鸽',
       avatar: 'https://oss.laf.run/ukw0y1-site/avatar.jpg',
     })
-    // 2/2 调用接口回来后设置token信息
-    // 这里用单token来模拟
-    tokenStore.setTokenInfo({
-      token: '123456',
-      expiresIn: 60 * 60 * 24 * 7,
-    })
+
     console.log(redirectUrl.value)
   }
   catch (error) {
