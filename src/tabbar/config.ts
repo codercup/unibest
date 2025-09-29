aaecf303715344e98549dac763e2fbd802a90a57
feige996 <1020102647@qq.com>
2025-09-29 20:14:30 +0800

    refactor(tabbar): 移除注释掉的关于页配置项并更新我的页注释

--- src/tabbar/config.ts
+++ src/tabbar/config.ts
@@ -32,12 +32,6 @@ export const nativeTabbarList: NativeTabBarItem[] = [
     pagePath: 'pages/index/index',
     text: '首页',
   },
-  // {
-  //   iconPath: 'static/tabbar/example.png',
-  //   selectedIconPath: 'static/tabbar/exampleHL.png',
-  //   pagePath: 'pages/about/about',
-  //   text: '关于',
-  // },
   {
     iconPath: 'static/tabbar/personal.png',
     selectedIconPath: 'static/tabbar/personalHL.png',
@@ -71,22 +65,14 @@ export const customTabbarList: CustomTabBarItem[] = [
     icon: 'i-carbon-home',
     // badge: 'dot',
   },
-  // {
-  //   text: '关于',
-  //   pagePath: 'pages/about/about',
-  //   // 注意 unocss 图标需要如下处理：（二选一）
-  //   // 1）在fg-tabbar.vue页面上引入一下并注释掉（见tabbar/index.vue代码第2行）
-  //   // 2）配置到 unocss.config.ts 的 safelist 中
-  //   iconType: 'unocss',
-  //   icon: 'i-carbon-code',
-  //   // badge: 10,
-  // },
   {
     pagePath: 'pages/me/me',
     text: '我的',
+    // 1）在fg-tabbar.vue页面上引入一下并注释掉（见tabbar/index.vue代码第2行）
+    // 2）配置到 unocss.config.ts 的 safelist 中
     iconType: 'unocss',
     icon: 'i-carbon-user',
-    // badge: 100,
+    // badge: 10,
   },
   // 其他类型演示
   // 1、uiLib
