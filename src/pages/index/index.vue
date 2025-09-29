fc04ecc097ff1fc245bc23e7da3e0a9b16cdbd3b
feige996 <1020102647@qq.com>
2025-09-29 20:22:37 +0800

    feat(首页): 添加前往示例页的按钮和跳转功能
    新增 gotoAbout 方法实现页面跳转功能，并在首页添加对应的按钮

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -25,6 +25,14 @@ console.log('index/index 首页打印了')
 onLoad(() => {
   console.log('测试 uni API 自动引入: onLoad')
 })
+
+// #region gotoAbout
+function gotoAbout() {
+  uni.navigateTo({
+    url: '/pages-sub/about/about',
+  })
+}
+// #endregion
 </script>
 
 <template>
@@ -95,6 +103,11 @@ onLoad(() => {
       UI组件官网：<text class="text-green-500">
         https://wot-design-uni.cn
       </text>
+    </view>
+    <view class="mt-4 text-center">
+      <wd-button type="primary" class="ml-2" @click="gotoAbout">
+        前往示例页
+      </wd-button>
     </view>
     <view class="h-6" />
   </view>
