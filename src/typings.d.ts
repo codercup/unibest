b7bf2512548be7d5d4084e1f4dff9e9663e55983
Utopia <greatauk11@gmail.com>
2025-10-29 11:07:08 +0800

    feat: 为 definePage 的参数扩展一些当前项目中特定的属性

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -30,6 +30,31 @@ declare global {
   }
 }
 
+// 扩展 @uni-helper/vite-plugin-uni-pages 的 definePage 参数类型
+declare module '@uni-helper/vite-plugin-uni-pages' {
+  interface UserPageMeta {
+    /**
+     * 使用 type: "home" 属性设置首页，其他页面不需要设置，默认为page
+     *
+     * 尽量保证一个项目 只有一个 这个配置，如果有多个，会按照字母顺序来排列，最终可能不是您想要的效果。
+     */
+    type?: 'home'
+    /**
+     * 页面布局类型, 模板默认只有 default, 如果在 src/layouts 下新增了 layout, 可以扩展当前属性
+     * @default 'default'
+     *
+     * 当前属性供 https://github.com/uni-helper/vite-plugin-uni-layouts 插件使用
+     */
+    layout?: 'default'
+    /**
+     * 是否从需要登录的路径中排除
+     *
+     * 登录授权(可选)：跟以前的 needLogin 类似功能，但是同时支持黑白名单，详情请见 src/router 文件夹
+     */
+    excludeLoginPath?: boolean
+  }
+}
+
 // patch uni 类型
 // 1. 补全 uni.hideToast() 的 options 类型
 // 2. 补全 uni.hideLoading() 的 options 类型
